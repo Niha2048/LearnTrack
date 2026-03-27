d7e961b36d81dfecf0740c85a589d66926dafc37
Niha2048 <errinirosha@gmail.com>
2026-03-27 12:40:31 +0530

    Added constants and enums

---
+++ src/com/airtribe/learntrack/constants/AppConstants.java
@@ -0,0 +1,14 @@
+package com.airtribe.learntrack.constants;
+
+public class AppConstants {
+
+    public static final String APP_NAME = "LearnTrack System";
+
+    public static final String INVALID_OPTION = "Invalid option. Please try again.";
+
+    public static final String STUDENT_NOT_FOUND = "Student not found!";
+    public static final String COURSE_NOT_FOUND = "Course not found!";
+    public static final String ENROLLMENT_NOT_FOUND = "Enrollment not found!";
+
+    public static final String SUCCESS = "Operation completed successfully!";
+}
